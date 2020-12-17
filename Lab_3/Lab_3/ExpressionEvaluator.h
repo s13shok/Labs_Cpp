e02449dd4f49b144b259dfe8a399a17399189ac4
Andrew Polous <55739120+s13shok@users.noreply.github.com>
2020-12-17 21:55:25 +0300

    Fix inheritance

--- Lab_3/Lab_3/ExpressionEvaluator.h
+++ Lab_3/Lab_3/ExpressionEvaluator.h
@@ -8,9 +8,6 @@ public:
 	ExpressionEvaluator(size_t n);
 	virtual ~ExpressionEvaluator();
 
-	virtual void logToScreen() const override = 0;
-	virtual void logToFile(const std::string& filename) const override = 0;
-
 	virtual double calculate() const = 0;
 
 	void setOperand(size_t pos, double value);
@@ -19,4 +16,3 @@ protected:
 	size_t n_;
 	double* operands_;
 };
-
